9ca2947078f7b3941f9ae6bbca50b8cf3d43a7a5
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-10-08 16:15:39 +0200

    Create books DB and load all data into all_books table in the DB using piplines

--- books/pipelines.py
+++ books/pipelines.py
@@ -7,16 +7,34 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import logging
+import sqlite3
 
 
-class BooksPipeline:
+class SQLitePipeline:
     def open_spider(self, spider):
-        logging.warning("SPIDER OPENDED FROM PIPELINE")
+        self.connection = sqlite3.connect("books.db")
+        self.c = self.connection.cursor()
+        self.c.execute('''
+            CREATE TABLE all_books(
+                name TEXT,
+                price TEXT,
+                available TEXT
+            )''')
+
+        self.connection.commit()
 
 
     def close_spider(self, spider):
-        logging.warning("SPIDER CLOSED FROM PIPELINE")
+        self.connection.close()
 
 
     def process_item(self, item, spider):
+        self.c.execute('''
+            INSERT INTO all_books (name, price, available) VALUES(?, ?, ?)
+            ''', (
+                item.get('name'),
+                item.get('price'),
+                item.get('availability')
+            ))
+        self.connection.commit()
         return item
